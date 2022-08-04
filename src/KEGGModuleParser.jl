f979f1a6daa30994067bac90b00894d4679d4f3a
HettieC <hester.chapman@btinternet.com>
2022-08-04 13:09:06 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.28

---
+++ src/KEGGModuleParser.jl
@@ -0,0 +1,5 @@
+module KEGGModuleParser
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using KEGGModuleParser
+using Test
+
+@testset "KEGGModuleParser.jl" begin
+    # Write your tests here.
+end
