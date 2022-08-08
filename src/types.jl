4f4277b970d7622af8dc80dbb29b6c02a8f017cd
HettieC <hester.chapman@btinternet.com>
2022-08-08 11:17:07 +0200

    type created

---
+++ src/types.jl
@@ -0,0 +1,10 @@
+struct KEGGModule
+    Entry::Dict{String,String}
+    Name::String
+    Definition::Vector{String}
+    Orthology::Dict{String,Vector{String}}
+    Class::Vector{String}
+    Pathway::Dict{String,String}
+    Reaction::Dict{String,String}
+    Compound::Dict{String,String}
+end
